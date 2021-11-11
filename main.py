98d87cc66cce8a47a25ea8daae27d8445317bca3
alves-Moises <moisesinho_ra@hotmail.com>
2021-11-11 00:17:00 -0300

    update man

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame, sys
 
+
 def main():
     pygame.init()
     size = width, height = 1080, 720
     screen = pygame.display.set_mode(size)
-    pygame.mixer.init()
+    pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=512, devicename="audio.wav")
+    audio = pygame.mixer.Channel.get_sound("audio.wav")
+    audio.play() 
 
-
+main()
