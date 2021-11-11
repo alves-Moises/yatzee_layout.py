2cf65b954c902d3d4fa78146023ccae56492cbf6
alves-Moises <moisesinho_ra@hotmail.com>
2021-11-11 17:20:57 -0300

    update main

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import pygame, sys
 
-
 def main():
     pygame.init()
     size = width, height = 1080, 720
     screen = pygame.display.set_mode(size)
-    pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=512, devicename="audio.wav")
-    audio = pygame.mixer.Channel.get_sound("audio.wav")
-    audio.play() 
 
+    dices = [pygame.image.load("/assets/1.jpg")]
+    dicerect = [dices[0].get_rect()]
+
+    
+    black = 0, 0, 0
+    red = 10, 0, 0
+    green = 0, 10, 0
+    blue = 0, 0, 10
+    cor = [black, red, green, blue]
+    while 1:
+
+
+            screen.fill(black)
+            pygame.display.flip()
 main()
