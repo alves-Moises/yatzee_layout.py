1183e71398687c2218c195e757ff95d5b29eb647
alves-Moises <moisesinho_ra@hotmail.com>
2021-11-16 01:13:36 -0300

    update mainj

---
+++ dice.py
@@ -0,0 +1,15 @@
+import pygame
+
+dice_1 = pygame.image.load('1.png')
+dice_2 = pygame.image.load('2.png')
+dice_3 = pygame.image.load('3.png')
+dice_4 = pygame.image.load('4.png')
+dice_5 = pygame.image.load('5.png')
+dice_6 = pygame.image.load('6.png')
+
+dicerect_1 = dice_1.get_rect()
+dicerect_2 = dice_2.get_rect()
+dicerect_3 = dice_3.get_rect()
+dicerect_4 = dice_4.get_rect()
+dicerect_5 = dice_5.get_rect()
+dicerect_6 = dice_6.get_rect()
--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import pygame, sys
+import dice as d
+
+pygame.init()
 
 def main():
-    pygame.init()
-    size = width, height = 1080, 720
+    size = width, height = 800, 600
+    # size = pygame.display.get_desktop_sizes()[0]
     screen = pygame.display.set_mode(size)
 
-    dices = [pygame.image.load("/assets/1.jpg")]
-    dicerect = [dices[0].get_rect()]
-
+    
+    print(pygame.display.get_desktop_sizes())
     
     black = 0, 0, 0
-    red = 10, 0, 0
-    green = 0, 10, 0
-    blue = 0, 0, 10
-    cor = [black, red, green, blue]
-    while 1:
+
+    # red = 10, 0, 0
+    # green = 0, 10, 0
+    # blue = 0, 0, 10
+    # cor = [black, red, green, blue]
+    
 
 
-            screen.fill(black)
-            pygame.display.flip()
+    game = True
+    while game:
+
+
+
+
+        screen.fill(black)
+        screen.blit(d.dice_1, d.dicerect_1)
+        d.dicerect_1 = d.dicerect_1.move(1, 10)
+
+
+        pygame.display.flip()
+        input('asd')
+
 main()
